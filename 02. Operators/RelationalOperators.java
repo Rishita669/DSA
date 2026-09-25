fc1caedfd671e6f504db422412ba092d32e691d6
Rishita Sharma <rishitasharma0130@gmail.com>
2026-09-25 23:30:40 +0530

    Add RelationalOperators.java

---
+++ 02. Operators/RelationalOperators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class RelationalOperators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        // Equals to [==]
+        System.out.println((a==b));
+        // Not equals to [!=]
+        System.out.println((a!=b));
+        // Greater than [>]
+        System.out.println((a>b));
+        // Less than [<]
+        System.out.println((a<b));
+        // Greter than or equal to [>=]
+        System.out.println((a>=b));
+        // Less than or equal to [<=]
+        System.out.println((a<=b));
+    }
+}
